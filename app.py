d441ac4b60267a1b97333858815edb7df1b7d9b4
devGabyAlves <gabriellyalves401@gmail.com>
2024-06-30 12:42:02 -0300

    Implementing Payment Confirmation and Notifying Customers of Confirmed Payments

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
 db.init_app(app)
 socketio = SocketIO(app)
 
+
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():
     data = request.get_json()
@@ -47,13 +48,39 @@ def get_image(file_name):
 
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
-def confirmation_pix():
+def pix_confirmation():
+
+    data = request.get_json()
+
+    if "bank_payment_id" not in data and "value" not in data:
+        return jsonify({"message": "Invalid payment data"}), 400
+
+    payment = Payment.query.filter_by(
+        bank_payment_id=data.get("bank_payment_id").first()
+        )
+
+    if not payment or payment.paid:
+        return jsonify({"message": "Payment not found"}), 404
+
+    if data.get("value") != payment.value:
+        return jsonify({"message": "Invalid payment data"}), 400
+
+    payment.paid = True
+    db.session.commit()
+    socketio.emit(f'payment-confirmed-{payment.id}')
+
     return jsonify({"message": "The payment has been confirmed"})
 
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
+
+    if payment.paid:
+        return render_template('confirmed_payment.html',
+                               payment_id=payment.id,
+                               value=payment.value
+                               )
 
     return render_template('payment.html',
                            payment_id=payment.id,
@@ -62,9 +89,11 @@ def payment_pix_page(payment_id):
                            qr_code=payment.qr_code,
                            )
 
+
 @socketio.on('connect')
 def handle_connect():
     print("Client connected to the server")
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
